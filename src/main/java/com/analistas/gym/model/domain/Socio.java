b62d4d1e41fcf260fff70c649a1a6b647abde8a2
SadSatan <santiagovalentixd@gmail.com>
2025-11-21 19:20:39 -0300

    Creación de las clases Rol, Socio y Usuario, ver si faltan campos

---
+++ src/main/java/com/analistas/gym/model/domain/Socio.java
@@ -0,0 +1,48 @@
+package com.analistas.gym.model.domain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "socios")
+public class Socio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "El nombre no puede estar vacío.")
+    @Size(min = 3, max = 25, message = "El nombre debe tener entre 3 y 25 caracteres.")
+    private String nombreCompleto;
+
+    @NotEmpty(message = "El DNI no puede estar vacío.")
+    @Size(min = 5, max = 8)
+    private String dni;
+
+    private LocalDate fechaNacimiento;
+
+    private String telefono;
+
+    private String direccion;
+
+    private LocalDateTime fechaAlta;
+
+    private LocalDateTime ultIngreso;
+
+    private LocalDateTime fechaVencimiento;
+
+    private Integer saldoPendiente;
+
+    private Boolean CuotaPaga;
+
+}
