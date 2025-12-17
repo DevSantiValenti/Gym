e0b629ab40c99d4f6489db9b1ebe9e49a55d8469
sasuale1 <velazcosalejandro@hotmail.com>
2025-12-17 19:13:22 -0300

    mejoras en el sweetalert 2

--- src/main/resources/static/js/confirmacionEliminar.js
+++ src/main/resources/static/js/confirmacionEliminar.js
@@ -1,17 +1,30 @@
-    function confirmarEliminacion(url) {
-    Swal.fire({
-        title: "¿Estás seguro?",
-        text: "Esta acción no se puede deshacer.",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Sí, eliminar",
-        cancelButtonText: "Cancelar"
-    }).then((result) => {
-        if (result.isConfirmed) {
-            window.location.href = url;
-        }
-    });
+function confirmarEliminacion(url) {
+  Swal.fire({
+    title: '¿Estas seguro?',
+    text: 'Esta acción no se puede deshacer',
+    icon: 'warning',
+
+    background: 'rgba(0,0,0,0.95)',
+    color: '#ffffff',
+
+    iconColor: '#ff005d',
+
+    showCancelButton: true,
+    confirmButtonText: 'ELIMINAR',
+    cancelButtonText: 'CANCELAR',
+
+    confirmButtonColor: '#ff005d',
+    cancelButtonColor: '#00ff9c',
+
+    customClass: {
+      popup: 'swal-neon',
+      title: 'swal-title-neon',
+      confirmButton: 'swal-btn-confirm',
+      cancelButton: 'swal-btn-cancel'
+    }
+  }).then((result) => {
+    if (result.isConfirmed) {
+      window.location.href = url;
+    }
+  });
 }
-    
